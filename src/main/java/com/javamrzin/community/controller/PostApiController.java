fb40556607597e6b2520f1ef220cf000b1985140
JavaMrZin <java.mr.zin@gmail.com>
2022-03-01 18:48:18 +0900

    Rest API Controller 생성
    - Postman을 이용한 GET, POST, PUT, DELETE 동작 확인

---
+++ src/main/java/com/javamrzin/community/controller/PostApiController.java
@@ -0,0 +1,56 @@
+package com.javamrzin.community.controller;
+
+import com.javamrzin.community.model.Post;
+import com.javamrzin.community.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PostApiController {
+
+    private final PostRepository postRepository;
+
+    @GetMapping("/posts")
+    List<Post> all(@RequestParam(required = false, defaultValue = "") String title
+            , @RequestParam(required = false, defaultValue = "") String content) {
+        if (StringUtils.hasText(title) || StringUtils.hasText(content)) {
+            return postRepository.findByTitleOrContent(title, content);
+        } else {
+            return postRepository.findAll();
+        }
+    }
+
+    @PostMapping("/posts")
+    Post newPost(@RequestBody Post newPost) {
+        return postRepository.save(newPost);
+    }
+
+    @GetMapping("/posts/{id}")
+    Post one(@PathVariable Long id) {
+        return postRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/posts/{id}")
+    Post replacePost(@RequestBody Post newPost, @PathVariable Long id) {
+        return postRepository.findById(id)
+                .map(post -> {
+                    post.setTitle(newPost.getTitle());
+                    post.setContent(newPost.getContent());
+                    return postRepository.save(post);
+                })
+                .orElseGet(() -> {
+                    newPost.setId(id);
+                    return postRepository.save(newPost);
+                });
+    }
+
+    @DeleteMapping("/posts/{id}")
+    void deletePost(@PathVariable Long id) {
+        postRepository.deleteById(id);
+    }
+}
--- src/main/java/com/javamrzin/community/repository/PostRepository.java
+++ src/main/java/com/javamrzin/community/repository/PostRepository.java
@@ -3,6 +3,11 @@ package com.javamrzin.community.repository;
 import com.javamrzin.community.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByTitle(String title);
+    List<Post> findByTitleOrContent(String title, String content);
 
 }
